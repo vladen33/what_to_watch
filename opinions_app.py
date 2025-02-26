15115b743aea60a26ddae06d58dfd3387b963271
Vladislav Vasilev <vladen21@yandex.ru>
2025-02-26 16:19:04 +0300

    dftywerty

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,43 @@
-from flask import Flask
+# what_to_watch/opinions_app.py
 
-app = Flask(__name__)
+from datetime import datetime
+from random import randrange
+
+# Импортируем функцию render_template():
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, static_folder='static')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db = SQLAlchemy(app)
+
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
 @app.route('/')
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    quantity = Opinion.query.count()
+    if not quantity:
+        return 'В базе данных записей нет.'
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
+    # Тут подключаем шаблон opinion.html:
+    return render_template('opinion.html', opinion=opinion)
+
+
+@app.route('/add')
+def add_opinion_view():
+    # Тут подключаем шаблон add_opinion.html:
+    return render_template('add_opinion.html')
+
 
 if __name__ == '__main__':
     app.run()
